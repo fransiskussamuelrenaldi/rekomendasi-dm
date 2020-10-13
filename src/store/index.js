a5a9dc96eab63d94178f9dbff69df154e0df7ddd
Paulina Davita <paulina.davita@dana.id>
2020-10-14 00:42:40 +0700

    feat: init store funcs

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// Mutations
+import { SET_PATIENT_DATA, SET_RECOMMENDATION_LIST } from './mutation-types'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    patientData: {
+      firstName: '',
+      lastName: '',
+      age: '',
+      gender: '',
+      weight: '',
+      address: ''
+    },
+    reccomendations: [],
+    rootCauseIdentification: []
   },
   mutations: {
+    [SET_PATIENT_DATA] (state, payload) {
+      Object.assign(state.patientData, payload)
+    },
+    [SET_RECOMMENDATION_LIST] (state, payload) {
+      state.reccomendations = payload
+    }
   },
   actions: {
+    setPatientData ({ commit, state }, payload) {
+      commit(SET_PATIENT_DATA, payload)
+    },
+    setRecommendations ({ commit, state }, payload) {
+      commit(SET_PATIENT_DATA, payload)
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
---
+++ src/store/mutation-types.js
@@ -0,0 +1,2 @@
+export const SET_PATIENT_DATA = 'setPatientData'
+export const SET_RECOMMENDATION_LIST = 'setRecommendationList'
