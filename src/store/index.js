2b94858381f019a3787657f2d1f090db80ea12cb
Paulina Davita <paulina.davita@dana.id>
2020-10-25 18:28:41 +0700

    feat: add general rec mutation, and px data

--- src/store/index.js
+++ src/store/index.js
@@ -2,20 +2,30 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 // Mutations
-import { SET_PATIENT_DATA, SET_RECOMMENDATION_LIST } from './mutation-types'
+import {
+  SET_PATIENT_DATA,
+  SET_RECOMMENDATION_LIST,
+  SET_GENERAL_REC
+} from './mutation-types'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     patientData: {
-      firstName: '',
-      lastName: '',
+      name: '',
       age: '',
       gender: '',
       weight: '',
-      address: ''
+      height: '',
+      address: '',
+      consultantType: '',
+      consultantName: '',
+      fastingBloodGlucose: '',
+      hba1c: '',
+      calcBmi: ''
     },
+    generalRecommendations: [],
     reccomendations: [],
     rootCauseIdentification: []
   },
@@ -25,6 +35,9 @@ export default new Vuex.Store({
     },
     [SET_RECOMMENDATION_LIST] (state, payload) {
       state.reccomendations = payload
+    },
+    [SET_GENERAL_REC] (state, payload) {
+      state.generalRecommendations = [...state.generalRecommendations, { ...payload }]
     }
   },
   actions: {
@@ -33,6 +46,9 @@ export default new Vuex.Store({
     },
     setRecommendations ({ commit, state }, payload) {
       commit(SET_PATIENT_DATA, payload)
+    },
+    setGeneralRec ({ commit, state }, payload) {
+      commit(SET_GENERAL_REC, payload)
     }
   },
   modules: {}
--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,2 +1,3 @@
 export const SET_PATIENT_DATA = 'setPatientData'
 export const SET_RECOMMENDATION_LIST = 'setRecommendationList'
+export const SET_GENERAL_REC = 'setGeneralRec'
