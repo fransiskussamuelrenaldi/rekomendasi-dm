124b35738903f32c7743ddb2b0239c77aa78e254
Paulina Davita <paulina.davita@dana.id>
2020-11-21 15:08:07 +0700

    feat: add drug table index and router

---
+++ src/views/drug-table.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="container w-full flex flex-wrap mx-auto px-2 pt-8 lg:pt-16 mt-16">
+      <div class="w-full p-8 mt-6 lg:mt-0 rounded shadow bg-white">
+        <div class="text-center m-auto left-side w-full mb-4">
+          <p class="pb-0 items-center break-normal text-gray-700 md:text-xl pb-4 font-bold"> Tabel Obat Diabetes </p>
+          <img src="/images/jenis-obat.png">
+        </div>
+        <div class="text-center m-auto left-side w-full mb-4">
+          <p class="pb-0 items-center break-normal text-gray-700 md:text-xl pb-4 font-bold"> Profil Obat-obatan Diabetes </p>
+          <img src="/images/jenis-obat-1.png">
+        </div>
+        <div class="text-center m-auto left-side w-full">
+          <p class="pb-0 items-center break-normal text-gray-700 md:text-xl pb-4 font-bold"> Tabel Kerja Insulin </p>
+          <img src="/images/insulin.png">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
